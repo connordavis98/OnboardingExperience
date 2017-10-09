3e4f5152290a830cd45ab3f231504e1e31d78459
Connor Davis <seafoamgreentele@gmail.com>
2017-10-09 11:04:54 -0700

    Initial adding of files

---
+++ OnboardApplication/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingExperience
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            Console.WriteLine("Hello! Thank you for using our Onboard Application!");
+            user.FirstName = AskQuestion("What is your first name?");
+            Console.WriteLine("Awesome! Your first name is " + user.FirstName);
+
+            user.LastName = AskQuestion("What is your last name?");
+            Console.WriteLine("Awesome! Your full name is " + user.FirstName + " " + user.LastName);
+
+            user.IsAccountOwner = AskBoolQuestion("Are you the account owner?");
+            Console.WriteLine("Awesome! You are account owner: " + user.IsAccountOwner);
+
+            user.PinNumber = AskPinNumber("What is your 4 digit PIN?", 4);
+            Console.WriteLine("Awesome! You entered: " + user.PinNumber);
+
+            Console.ReadLine();
+
+        }
+
+        public static string AskQuestion(string question)
+        {
+            Console.WriteLine(question);
+            return Console.ReadLine();
+
+        }
+        static bool AskBoolQuestion(string question)
+        {
+            var isAccountOwner = false;
+
+            while (!isAccountOwner)
+            {
+                var response = AskQuestion(question + "(y/n)");
+
+                if (response == "y")
+                {
+                    isAccountOwner = true;
+                }
+                else
+                {
+                    Console.Write("You must be the account owner to proceed. ");
+                }
+            }
+
+            return isAccountOwner;
+        }
+
+        static string AskPinNumber(string question, int length)
+        {
+            string numberString = null;
+            
+            while (numberString == null)
+            {
+                var response = AskQuestion(question);
+
+                if (response.Length == length && Int32.TryParse(response, out int possiblereturn))
+                {
+                    numberString = response;
+                }
+            }
+
+            return numberString;
+        }
+    }
+}
---
+++ OnboardApplication/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingExperience
+{
+    public class User
+    { 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAccountOwner { get; set; }
+        public string PinNumber { get; set;  }
+    }
+}
